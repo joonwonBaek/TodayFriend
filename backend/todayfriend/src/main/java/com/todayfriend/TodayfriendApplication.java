134b0da36c8fca3aa81907b1cb8d7e46cec7f744
sseq007 <sseq007@naver.com>
2024-01-23 09:39:44 +0900

    feat : springboot 환경 설정

---
+++ backend/todayfriend/src/main/java/com/todayfriend/TodayfriendApplication.java
@@ -0,0 +1,13 @@
+package com.todayfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayfriendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodayfriendApplication.class, args);
+    }
+
+}
---
+++ backend/todayfriend/src/test/java/com/todayfriend/TodayfriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todayfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayfriendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
