55ce384f5f00b2ada509d430783b31bdd3b378a7
Baek <woonn97@gmail.com>
2024-03-05 00:31:55 +0900

    [🎨style]: BottomNavBar 스타일링

---
+++ frontend/src/components/BottomNavBar/style.ts
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+import { theme } from '@/styles/Theme';
+
+export const BottomNavBarWrapper = styled.div`
+  position: fixed;
+  max-width: ${({ theme }) => theme.device.mobile};
+  width: 100%;
+  left: 50%;
+  transform: translate(-50%, 0);
+  display: flex;
+  height: 6rem;
+  justify-content: space-around;
+  align-items: center;
+  background-color: white;
+  bottom: 0;
+  padding: 2rem;
+  border-top: 1px solid ${theme.colors.lightGray};
+`;
+
+export const LinkWrapper = styled(Link)<{ active: string }>`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  color: ${({ active }) =>
+    active === 'true' ? theme.colors.primary : theme.colors.gray};
+  transition: color 0.3s;
+  &:hover {
+    color: ${theme.colors.primary};
+  }
+`;
